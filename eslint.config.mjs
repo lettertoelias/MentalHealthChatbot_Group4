b8cc1d8c23561440c2e8242c24c8b06385350b2e
idlesummer <09louisthebob@gmail.com>
2025-10-13 00:27:29 +0800

    init

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,40 +1,25 @@
-import { dirname } from 'path'
-import { fileURLToPath } from 'url'
-import { FlatCompat } from '@eslint/eslintrc'
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-})
+});
 
 const eslintConfig = [
-  // === Next.js base rules ===
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
-
-  // === Custom ignores ===
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     ignores: [
-      'node_modules/**',
-      '.next/**',
-      'out/**',
-      'build/**',
-      'next-env.d.ts',
+      "node_modules/**",
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
     ],
   },
+];
 
-  // === Custom style rules ===
-  {
-    rules: {
-      'comma-dangle': ['warn', 'always-multiline'],       // enforce for multiline objects/arrays
-      'eol-last': ['warn', 'always'],                     // Enforce exactly one newline at EOF
-      'jsx-quotes': ['warn', 'prefer-double'],            // Double quotes for JSX
-      'object-curly-spacing': ['warn', 'always'],         // spacing / formatting consistency
-      quotes: ['warn', 'single', { avoidEscape: true }],  // Single quotes for JS/TS
-      semi: ['warn', 'never'],                            // No semicolons
-    },
-  },
-]
-
-export default eslintConfig
+export default eslintConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
-import Image from 'next/image'
-
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -40,7 +39,7 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deplow now
+            Deploy now
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
@@ -100,5 +99,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
+  );
 }
